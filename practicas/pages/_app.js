1c57a532df8f78de8455395ba027151fb2050013
invaderzon <159202214+invaderzon@users.noreply.github.com>
2025-10-19 16:21:05 -0600

    Delete practicas directory

--- practicas/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import "@/styles/globals.css";
-import '../styles/style.css';
-import '../styles/navbar-footer.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
